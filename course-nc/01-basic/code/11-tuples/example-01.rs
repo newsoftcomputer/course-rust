88df0eb27f32eb138d93f57511f05e0693df3fb1
Andres Giraldo <andresganc@gmail.com>
2023-07-31 21:19:21 -0500

    New: Control Flow: For Range, If Else, Loop, Match, While. Mod: Tuples

--- course-nc/01-basic/code/11-tuples/example-01.rs
+++ course-nc/01-basic/code/11-tuples/example-01.rs
@@ -1,8 +1,15 @@
 
 fn main() {
-    // Declare a tuple of three elements
-    let tuple_e = ('E', 5i32, true);
+    // Declare a tuple of three elements with values directly
+    let tuple_1 = ('E', 5, true);
 
     // Use tuple indexing and show the values of the elements in the tuple
-    println!("Is '{}' the {}th letter of the alphabet? {}", tuple_e.0, tuple_e.1, tuple_e.2);
+    println!("Is '{}' the {} the letter of the alphabet? {}", tuple_1.0, tuple_1.1, tuple_1.2);
+    
+
+    // Declare tuple with types and assign value after
+    let tuple_2: (i32, &str, f64, bool) = (123456, "Wallet1", 120.5, true);
+    
+    println!("i32: {}, &str: {}, f64: {}, Boolean: {}", tuple_2.0, tuple_2.1, tuple_2.2, tuple_2.3);
+
 }

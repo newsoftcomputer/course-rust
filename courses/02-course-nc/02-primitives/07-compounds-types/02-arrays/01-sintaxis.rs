c5ab11f7ce91000b4cee66eeb12cbc065e70a734
Andres Giraldo <andresganc@gmail.com>
2024-01-25 23:22:52 -0500

    Mod: Course NC: Compounds Types

---
+++ courses/02-course-nc/02-primitives/07-compounds-types/02-arrays/01-sintaxis.rs
@@ -0,0 +1,27 @@
+
+fn main() {
+
+    // Declaration 01
+    let a = [1, 2, 3, 4, 5];
+
+    // Declaration 02
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"
+              ];
+
+
+    // Declaration 03
+    let b: [i32; 5] = [1, 2, 3, 4, 5];
+
+    
+    // Declaration 04
+    let c = [3; 5];     // out: let a = [3, 3, 3, 3, 3];
+
+   
+    // Declaration 05
+    let d = [1, 2, 3, 4, 5];
+    
+    let first = d[0];
+    let second = d[1];
+    
+}

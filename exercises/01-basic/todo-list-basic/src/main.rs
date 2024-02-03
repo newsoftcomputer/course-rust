97aeff073afa8c7d4159997676f5df47b0fef7c0
Andres Giraldo <andresganc@gmail.com>
2024-02-03 12:21:19 -0500

    Mod: Exercises: Todo List Basic

--- exercises/01-basic/todo-list-basic/src/main.rs
+++ exercises/01-basic/todo-list-basic/src/main.rs
@@ -1,3 +1,39 @@
+
+use std::io;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("TODO LIST BASIC");
+    println!("New Task");
+    println!("Show Task");
+
+    loop {
+        
+        let stdin = io::stdin();
+
+        let action = stdin.lock().lines().next().unwrap().unwrap();
+
+        if action == "0" {
+            break
+        }
+
+        println!("Value: {}", action)
+
+    }
+
 }
+
+
+fn showTasks() {
+
+    println!("Show task"); 
+
+}
+
+fn addTask() {
+    
+    println!("Added Task");
+
+}
+

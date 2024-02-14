862ef2470f693f5c3824fc114dca30c3c98c7d8e
Andres Giraldo <andresganc@gmail.com>
2024-02-13 21:53:56 -0500

    Mod: Exercises Todo List Advanced File

--- exercises/01-basic/todo-list-advanced-file/src/main.rs
+++ exercises/01-basic/todo-list-advanced-file/src/main.rs
@@ -1,3 +1,18 @@
+
+use crossterm::style::Stylize;
+use crossterm::style::Attribute;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("{}", "Bold".bold());
+    println!("{}", "Underlined".underlined());
+    println!("{}", "Negative".negative());
+
+    println!(
+        "{} Underlined {} No Underline",
+        Attribute::Underlined,
+        Attribute::NoUnderline
+    );
+
 }

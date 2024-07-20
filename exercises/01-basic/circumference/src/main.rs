19d847e9cc007bb80ec926c4bb856919ddee7df8
Andres Giraldo <andresganc@gmail.com>
2024-07-20 11:45:59 -0500

    Exerrcises: Circumference

---
+++ exercises/01-basic/circumference/src/main.rs
@@ -0,0 +1,9 @@
+
+fn main() {
+
+    const PI: f32 = 3.141592;
+    let radio: f32 = 10.0;
+    let circumference: f32 = 2.0 * PI * radio;    
+    println!("The circumference is: {}", circumference);
+
+}

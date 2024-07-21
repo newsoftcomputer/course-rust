1454707ddb48b2dcad70d446f8a0f290165df3c9
Andres Giraldo <andresganc@gmail.com>
2024-07-21 10:42:06 -0500

    Mod: Course NC: Float 16 & Float 128

--- courses/02-course-nc/02-primitives/02-float/02-float.rs
+++ courses/02-course-nc/02-primitives/02-float/02-float.rs
@@ -1,8 +1,17 @@
 
 fn main() {
-    let number = 4.0;      // compiler infers the value to use the default type f64
-    let number_32: f32 = 5.0; // type f32 specified via annotation
-    let number_64: f64 = 3.0; // type f64 specified via casting
+    
+    let float = 4.0;      // compiler infers the value to use the default type f64
+    let float_32: f32 = 5.0; // type f32 specified via annotation
+    let float_64: f64 = 3.0; // type f64 specified via casting
+    
+    println!("Float infere to 64Bits: {}, Float 32Bits: {}, Float 64Bits: {}", float, float_32, float_64);    
 
-    println!("Float infere to 64Bits: {}, Float 32Bits: {}, Float 64Bits: ", number, number_32, number_64);    
+    /* 
+    Experimental
+
+    f16
+    f128
+    
+    */
 }

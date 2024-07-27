65533cac62bfa42e55ea272cb152f313126f3d23
Andres Giraldo <andresganc@gmail.com>
2024-07-26 19:34:12 -0500

    Mod: Courses: Primitives: Char

--- courses/02-course-nc/02-primitives/04-char/04-char.rs
+++ courses/02-course-nc/02-primitives/04-char/04-char.rs
@@ -1,9 +1,10 @@
 
 fn main() {
+    
     let uppercase_s = 'S';
-    let lowercase_f = 'f';
-    let smiley_face = '😃';
-    // let hearth = '❤️';
+    let lowercase_f: char = 'f';
+    let smiley_face: char = '😃';
+    // let hearth: char = '❤️';
 
     // println!("Uppercase: {uppercase_s}, Lowercase: {lowercase_f}, SmileyFace: {smiley_face}, Hearth: {hearth}");
     println!("Uppercase: {uppercase_s}, Lowercase: {lowercase_f}, SmileyFace: {smiley_face}");

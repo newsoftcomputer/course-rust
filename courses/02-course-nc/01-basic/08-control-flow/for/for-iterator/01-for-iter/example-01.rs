4c2a5436518f9281be00324f2956b23391bcd290
Andres Giraldo <andresganc@gmail.com>
2024-07-30 22:53:24 -0500

    New: Course NC: Control Flow: For Iterator, For Range

---
+++ courses/02-course-nc/01-basic/08-control-flow/for/for-iterator/01-for-iter/example-01.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+
+    let array01 = [34, 67, 89, 65, 99];
+
+    for i: &i32 in array01.iter() {
+        println!("{}", i);
+    }
+
+
+}
--- courses/02-course-nc/01-basic/08-control-flow/for/for-range/01-for-range.rs
+++ courses/02-course-nc/01-basic/08-control-flow/for/for-range/01-for-range.rs
@@ -1,11 +1,46 @@
 
+/*
+
+FOR AND RANGE
+
+The for in construct can be used to iterate through an Iterator. One of the easiest ways to create an iterator is to use the range notation a..b. This yields values from a (inclusive) to b (exclusive) in steps of one.
+
+Let's write FizzBuzz using for instead of while.
+
+*/
+
 fn main() {
 
-    let array01 = [34, 67, 89, 65, 99];
-
-    for i: &i32 in array01.iter() {
-        println!("{}", i);
+    // `n` will take the values: 1, 2, ..., 100 in each iteration
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
     }
 
+    // Alternatively, a..=b can be used for a range that is inclusive on both ends. The above can be written as:
 
+    fn main() {
+        // `n` will take the values: 1, 2, ..., 100 in each iteration
+        for n in 1..=100 {
+            if n % 15 == 0 {
+                println!("fizzbuzz");
+            } else if n % 3 == 0 {
+                println!("fizz");
+            } else if n % 5 == 0 {
+                println!("buzz");
+            } else {
+                println!("{}", n);
+            }
+        }
+    }
+    
 }
+
+

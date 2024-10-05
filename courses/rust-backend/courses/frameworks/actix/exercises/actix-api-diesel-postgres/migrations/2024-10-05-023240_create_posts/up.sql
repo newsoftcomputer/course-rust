1a091f8df599e5c2a379dd40c747b5c0678a28cd
andresganc <andresganc@gmail.com>
2024-10-04 22:19:21 -0500

    Mod: Rust Backend: Actix: Exercises Actix API Diesel PostgreSQL: SQL table users + uuid

--- courses/rust-backend/courses/frameworks/actix/exercises/actix-api-diesel-postgres/migrations/2024-10-05-023240_create_posts/up.sql
+++ courses/rust-backend/courses/frameworks/actix/exercises/actix-api-diesel-postgres/migrations/2024-10-05-023240_create_posts/up.sql
@@ -1,28 +1,32 @@
 -- Your SQL goes here
 
+
+
+
 CREATE TABLE users (
-    id_users UUID NOT NULL,
+    id_users uuid NOT NULL,
     first_name VARCHAR,
     last_name VARCHAR,
     email VARCHAR,
-    status BOOLEAN NOT NULL DEFAULT FALSE
-)
+    status BOOLEAN NOT NULL DEFAULT FALSE,
+    -- CONSTRAINT "users" PRIMARY KEY (id_users)
+);
 
-TABLESPACE pg_default;
+-- TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS users
-  OWNER to postgres;
+--ALTER TABLE if EXISTS users
+--  OWNER to postgres;
 
 
-CREATE TABLE posts (
-  id_posts SERIAL NOT NULL,
-  title VARCHAR,
-  body TEXT,
-  published BOOLEAN NOT NULL DEFAULT FALSE
-  CONSTRAINT "posts" PRIMARY KEY (id_posts)
-)
+--CREATE TABLE posts (
+--    id_posts SERIAL PRIMARY KEY NOT NULL,
+--    title VARCHAR,
+--    body TEXT,
+--    published BOOLEAN NOT NULL DEFAULT FALSE
+    -- CONSTRAINT "posts" PRIMARY KEY (id_posts)
+);
 
-TABLESPACE pg_default;
+-- TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS posts
-  OWNER to postgres;
+--ALTER TABLE IF EXISTS posts
+--  OWNER to postgres;

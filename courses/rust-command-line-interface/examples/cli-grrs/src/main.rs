3bfc351e271a655e7cf9ca0f1f6607bd93b790a3
Andres Giraldo <andresganc@gmail.com>
2024-10-11 23:19:58 -0500

    New: Rust CLI: Example CLI GRRS Ok

--- courses/rust-command-line-interface/examples/cli-grrs/src/main.rs
+++ courses/rust-command-line-interface/examples/cli-grrs/src/main.rs
@@ -10,17 +10,24 @@ struct Cli {
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("No Pattern given");
-    let path = std::env::args().nth(2).expect("No path given");
+    // CODE WITHOUT LIBRARY CLAP
+    // let pattern = std::env::args().nth(1).expect("No Pattern given");
+    // let path = std::env::args().nth(2).expect("No path given");
 
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    // println!("Pattern: {:?}, path {:?}", args.pattern, args.path);
 
-    println!("Pattern: {:?}, path {:?}", args.pattern, args.path);
+    // CODE WHIT LIBRARY CLAP
 
-    implementation01()
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    // implementation01()
 }
 
 fn implementation01() {
